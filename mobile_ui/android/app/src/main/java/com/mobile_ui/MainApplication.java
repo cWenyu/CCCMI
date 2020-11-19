8fce721cae020b3ed753026c1d23e9fa55f8e331
Wenyu Shi <d00198805@student.dkit.ie>
2020-11-19 01:09:39 +0000

    setup react-native-maps configs

--- mobile_ui/android/app/src/main/java/com/mobile_ui/MainApplication.java
+++ mobile_ui/android/app/src/main/java/com/mobile_ui/MainApplication.java
@@ -2,6 +2,7 @@ package com.mobile_ui;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -10,6 +11,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Arrays;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.facebook.react.shell.MainReactPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,11 +26,10 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new MapsPackage()
+            );
         }
 
         @Override
