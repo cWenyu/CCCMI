3e7177debb7579a17db158a8d63d513eddc8da05
East1036 <tunjing998@gmail.com>
2021-02-27 17:38:06 +0000

    Add Map Page

--- Aquality_Two_Backend_Server/aquality_admin/urls.py
+++ Aquality_Two_Backend_Server/aquality_admin/urls.py
@@ -6,5 +6,6 @@ app_name= 'aquality_admin'
 urlpatterns = [
     path('',views.index,name='indexPage'),
     path('index',views.index,name='indexPage'),
+    path('maps',views.maps,name='mapPage'),
     path('', include('django.contrib.auth.urls'))
 ]
