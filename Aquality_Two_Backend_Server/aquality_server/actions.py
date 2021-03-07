38212d75c1b9bf9d23ff587ec6e0cdafbe23f632
East1036 <tunjing998@gmail.com>
2021-03-07 16:33:24 +0000

    Move Comment

--- Aquality_Two_Backend_Server/aquality_server/actions.py
+++ Aquality_Two_Backend_Server/aquality_server/actions.py
@@ -7,25 +7,25 @@ CATCHMENT_API_KEY = 'catchment/'
 SUB_CATCHMENT_API_KEY = '/subcatchment/'
 WATERBODY_API_KEY = 'waterbody/'
 
-#Get JsonResult From targetUrl
 def get_json_from(target_url):
+    """Get JsonResult From targetUrl"""
     response = requests.get(target_url)
     return response.json()
 
-#Get all CatchmentList From API
 def get_catchment_list():
+    '''Get all CatchmentList From API'''
     target_url = CATCHMENTS_URL + CATCHMENT_API_KEY
     json_data = get_json_from(target_url)
     return json_data['Catchments']
 
-#Get One CatchmentDetai By Catchment Code
 def get_catchment_detail(code):
+    '''Get One CatchmentDetai By Catchment Code'''
     target_url = CATCHMENTS_URL + CATCHMENT_API_KEY + code
     json_data = get_json_from(target_url)
     return json_data
 
-#Get All Catchment Detail By Using All Catchment 
 def get_catchment_detail_list():
+    '''Get All Catchment Detail By Using All Catchment '''
     catchment_list = get_catchment_list()
     catchment_data_list = []
     for catchment in catchment_list:
@@ -33,20 +33,20 @@ def get_catchment_detail_list():
         catchment_data_list.append(get_json_from(target_url))
     return catchment_data_list
 
-#Get Subcatchment Detail by SubcatchmentCode
 def get_subcatchment_detail(subcatchment_code):
+    '''Get Subcatchment Detail by SubcatchmentCode'''
     target_url = CATCHMENTS_URL + CATCHMENT_API_KEY + subcatchment_code.split('_')[0]+ SUB_CATCHMENT_API_KEY + subcatchment_code
     json_data = get_json_from(target_url)
     return json_data
 
-#Get Waterbody Detail by its code
 def get_water_body_detail(waterbody_code):
+    '''Get Waterbody Detail by its code'''
     target_url = CATCHMENTS_URL + WATERBODY_API_KEY + waterbody_code
     json_data = get_json_from(target_url)
     return json_data
 
-#Get All Waterbody Code 
 def get_all_water_body_code():
+    '''Get All Waterbody Code '''
     catchment_list = get_catchment_detail_list()
     waterbody_code_list = []
     for catchment in catchment_list:
@@ -55,8 +55,8 @@ def get_all_water_body_code():
                 waterbody_code_list.append(waterbody["Code"])
     return waterbody_code_list
             
-#Get All Waterbody Details List
 def get_all_water_body_details():
+    '''Get All Waterbody Details List'''
     waterbody_code_list = get_all_water_body_code()
     waterbody_code_list = list(dict.fromkeys(waterbody_code_list))
     waterbody_detail_list = []
@@ -65,13 +65,13 @@ def get_all_water_body_details():
         waterbody_detail_list.append(get_json_from(target_url))
     return waterbody_detail_list
 
-#Get Waterbody Detail By Its Code
 def get_one_water_details(waterbody_code):
+    '''Get Waterbody Detail By Its Code'''
     target_url = CATCHMENTS_URL + WATERBODY_API_KEY + waterbody_code
     return get_json_from(target_url)
 
-#Save All RiverList into Database
 def save_river_list_to_database(waterbody_list):
+    '''Save All RiverList into Database'''
     for waterbody in waterbody_list:
         water = River(
                 river_code=waterbody.get("Code"),
@@ -88,8 +88,8 @@ def save_river_list_to_database(waterbody_list):
                 )
         water.save()
 
-# Load All Data From WFA, Save to DB
 def save_river_list_to_database_from_wfa():
+    ''' Load All Data From WFA, Save to Database'''
     waterbodylist = get_all_water_body_details()
     save_river_list_to_database(waterbodylist)
     return 'Done'
