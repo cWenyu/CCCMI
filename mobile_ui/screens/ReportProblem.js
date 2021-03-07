02641baaf4c1c30212533132c0a7aa9954e0e01d
Patrick <patricklee0221@gmail.com>
2021-03-07 15:58:11 +0000

    edit words

--- mobile_ui/screens/ReportProblem.js
+++ mobile_ui/screens/ReportProblem.js
@@ -1,32 +1,41 @@
 import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet, Image, Picker} from 'react-native';
+import {View, Text, Button, StyleSheet, Image, Picker, TextInput} from 'react-native';
 
 import {useTheme} from '@react-navigation/native';
 
 const ReportProblem = ({navigation}) => {
   const {colors} = useTheme();
-  const [selectedValue, setSelectedValue] = useState("insectwrong");
+  const [selectedValue, setSelectedValue] = useState("insect");
+
+
+  
+
   return (
     <View style={styles.container}>
       <Image
         style={{width: 100,height: 100}}
         source={require('../assets/insects/caenis.jpg')}
       />
+      <Text>From AI Analysis</Text>
+      <Text>Insect Name: Caenis</Text>
       <Text>Count: 2</Text>
-      <Text>What went wrong?</Text>
-      <Text>Choose the reason:</Text>
+      <Text>What went wrong? Choose the reason:</Text>
+      
       <Picker
         selectedValue={selectedValue}
-        style={{ height: 50, width: 300 }}
+        style={{ height: 50, width: 300}}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
       >
           {/* <Picker.Item label="Choose one ..." value="default"/> */}
-        <Picker.Item label="The insect is wrong." value="insectwrong" />
-        <Picker.Item label="The count is wrong." value="countwrong" />
+        <Picker.Item label="The insect name is wrong." value="insect" />
+        <Picker.Item label="The count is wrong." value="count" />
         <Picker.Item label="Some bug." value="bug" />
       </Picker>
 
       <Button title='send' onPress={()=> navigation.navigate('AnalyzeInsect')}/>
+
+      
+
     </View>
   );
 };
