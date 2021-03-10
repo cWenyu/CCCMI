7e695167c5d5be9b7fd2c72ea53c6af2d0684f38
East1036 <tunjing998@gmail.com>
2021-03-10 23:24:49 +0000

    Adding Detail to Model

--- Aquality_Two_Backend_Server/aquality_admin/static/aquality_admin/js/maps.js
+++ Aquality_Two_Backend_Server/aquality_admin/static/aquality_admin/js/maps.js
@@ -1,15 +1,30 @@
 var map, heatmap, markerclusterer, markers;
 var ireland = { lat: 53.4624, lng: -7.6921 };
 
-async function getJSON() {
+async function getSampleRecordListJSON() {
   return fetch('/aquality_server/samplerecord/?username=userzzz')
     // return fetch('https://cccmi-aquality.tk/aquality_server/samplerecord/?username=userzzz')
     .then((response) => response.json())
     .then((responseJson) => { return responseJson });
 };
 
+async function getSampleRecordJSON(sample_id_in) {
+  var bodyFormData = new FormData()
+  bodyFormData.append('sample_id', sample_id_in)
+  return fetch('/aquality_server/sampledetail',
+    {
+      method: 'POST',
+      body: bodyFormData,
+      
+    }
+  )
+    // return fetch('https://cccmi-aquality.tk/aquality_server/samplerecord/?username=userzzz')
+    .then((response) => response.json())
+    .then((responseJson) => { return responseJson });
+}
+
 async function getRiver(array) {
-  const data = await getJSON()
+  const data = await getSampleRecordListJSON()
   for (let i = 0; i < data.length; i++) {
     river = data[i].sample_river;
     array.push({ lat: river.latitude, lng: river.longitude })
@@ -35,16 +50,16 @@ async function initMap() {
       '<div id="content">' +
       '<div id="siteNotice">' +
       "</div>" +
-      '<h2 id="firstHeading" class="firstHeading">ID : '+sampleData.sample_id+'</h2>' +
+      '<h2 id="firstHeading" class="firstHeading">ID : ' + sampleData.sample_id + '</h2>' +
       '<div id="bodyContent">' +
-      "<p>Sample Date  : "+sampleData.sample_date+"</p>" +
-      "<p>Sample Score : "+sampleData.sample_score+"</p>" +
-      "<p>Sample User  : "+sampleData.sample_user+"</p>" +
-      "<p>Sample Ph    : "+sampleData.sample_ph+"</p>" +
-      "<p>Sample Temp  : "+sampleData.sample_tmp+"</p>" +
-      "<p>Sample River : "+sampleData.sample_river.river_name+"</p>" +
-      "<p>Sample Coor  : "+JSON.stringify(sampleData.coordinates)+"</p>" +
-      "<p>Click Here for More Details"+"</p>" +
+      "<p>Sample Date  : " + sampleData.sample_date + "</p>" +
+      "<p>Sample Score : " + sampleData.sample_score + "</p>" +
+      "<p>Sample User  : " + sampleData.sample_user + "</p>" +
+      "<p>Sample Ph    : " + sampleData.sample_ph + "</p>" +
+      "<p>Sample Temp  : " + sampleData.sample_tmp + "</p>" +
+      "<p>Sample River : " + sampleData.sample_river.river_name + "</p>" +
+      "<p>Sample Coor  : " + JSON.stringify(sampleData.coordinates) + "</p>" +
+      "<button onclick='setModel(" + sampleData.sample_id + ")' type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#exampleModal'>Click Here for More Details</button>" +
       "</div>" +
       "</div>";
 
@@ -83,4 +98,9 @@ function toggleHeatmap() {
     heatmap.setMap(map)
     markerclusterer.clearMarkers()
   }
+}
+
+async function setModel(sample_id) {
+    var sampleDetail = await getSampleRecordJSON(sample_id)
+    document.getElementById('ModelContent').innerHTML =JSON.stringify(sampleDetail)
 }
