2586233a71aebe5e8cd8a8735497217376ecf4a0
East1036 <tunjing998@gmail.com>
2021-03-10 18:57:50 +0000

    Adding Data

--- Aquality_Two_Backend_Server/aquality_admin/static/aquality_admin/js/maps.js
+++ Aquality_Two_Backend_Server/aquality_admin/static/aquality_admin/js/maps.js
@@ -1,71 +1,85 @@
-var map,heatmap,markerclusterer,markers;
+var map, heatmap, markerclusterer, markers;
 var ireland = { lat: 53.4624, lng: -7.6921 };
 
-async function getJSON(){
+async function getJSON() {
   return fetch('/aquality_server/samplerecord/?username=userzzz')
-  // return fetch('https://cccmi-aquality.tk/aquality_server/samplerecord/?username=userzzz')
-      .then((response)=>response.json())
-      .then((responseJson)=>{return responseJson});
+    // return fetch('https://cccmi-aquality.tk/aquality_server/samplerecord/?username=userzzz')
+    .then((response) => response.json())
+    .then((responseJson) => { return responseJson });
 };
 
-async function getRiver(array){
-    const data = await getJSON()
-    for(let i = 0 ; i < data.length ;i++)
-    {
-      river = data[i].sample_river;
-      array.push({lat: river.latitude, lng : river.longitude})
-      data[i].coordinates = {lat: river.latitude, lng : river.longitude}
-    }
-    console.log(data)
-    return data
+async function getRiver(array) {
+  const data = await getJSON()
+  for (let i = 0; i < data.length; i++) {
+    river = data[i].sample_river;
+    array.push({ lat: river.latitude, lng: river.longitude })
+    data[i].coordinates = { lat: river.latitude, lng: river.longitude }
+  }
+  console.log(data)
+  return data
 }
 
 //  
 async function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: ireland,
-        zoom: 7
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: ireland,
+    zoom: 7
+  });
+
+  var locations = [];
+  var sampleDatas = await getRiver(locations);
+
+
+  markers = sampleDatas.map((sampleData) => {
+    var contentWithLayout =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h2 id="firstHeading" class="firstHeading">ID : '+sampleData.sample_id+'</h2>' +
+      '<div id="bodyContent">' +
+      "<p>Sample Date  : "+sampleData.sample_date+"</p>" +
+      "<p>Sample Score : "+sampleData.sample_score+"</p>" +
+      "<p>Sample User  : "+sampleData.sample_user+"</p>" +
+      "<p>Sample Ph    : "+sampleData.sample_ph+"</p>" +
+      "<p>Sample Temp  : "+sampleData.sample_tmp+"</p>" +
+      "<p>Sample River : "+sampleData.sample_river.river_name+"</p>" +
+      "<p>Sample Coor  : "+JSON.stringify(sampleData.coordinates)+"</p>" +
+      "<p>Click Here for More Details"+"</p>" +
+      "</div>" +
+      "</div>";
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentWithLayout
+    })
+    var marker = new google.maps.Marker({
+      position: sampleData.coordinates,
+      title: sampleData.sample_user
     });
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    })
+    return marker
+  });
 
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const heatmapdata = locations.map((location) => {
+    return new google.maps.LatLng(location.lat, location.lng);
+  });
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapdata
+  });
 
-    var locations = [];
-    var sampleDatas = await getRiver(locations);
-    
-
-    markers = sampleDatas.map((sampleData) => {
-        var infowindow = new google.maps.InfoWindow({
-          content:sampleData.sample_date
-        })
-        var marker =  new google.maps.Marker({
-          position: sampleData.coordinates,
-          title: sampleData.sample_user
-        });
-        marker.addListener("click",()=>{
-          infowindow.open(map,marker);
-        })
-        return marker
-      });
-
-    const heatmapdata = locations.map((location) => {
-        return new google.maps.LatLng(location.lat,location.lng);
-    });
-    heatmap = new google.maps.visualization.HeatmapLayer({
-      data: heatmapdata
-    });
-
-    markerclusterer = new MarkerClusterer(map, markers, {
-      imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    });
+  markerclusterer = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
 }
 
 function toggleHeatmap() {
-  if(heatmap.getMap()){
+  if (heatmap.getMap()) {
     heatmap.setMap(null)
     markerclusterer.addMarkers(markers)
   }
-  else{
+  else {
     heatmap.setMap(map)
     markerclusterer.clearMarkers()
   }
