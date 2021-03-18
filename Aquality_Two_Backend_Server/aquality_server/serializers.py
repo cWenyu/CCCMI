7e7be162b41ed78b5a0a045cbe78d50d3e6ec814
IllyaMallon <56591546+IllyaMallon@users.noreply.github.com>
2021-03-18 21:45:01 +0000

    Modified serializers
    Contains connection to the sample record

--- Aquality_Two_Backend_Server/aquality_server/serializers.py
+++ Aquality_Two_Backend_Server/aquality_server/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import River, Data, UserAccount, Insect, InsectGroup, SampleRecord, \
-    SampleRecordInsectDetail, User,AllInsectUserUpload, RiverEnvironmentImage, ReportProblemRecord
+    SampleRecordInsectDetail, User, AllInsectUserUpload, RiverEnvironmentImage, ReportProblemRecord
 
 
 class RiverSerializer(serializers.HyperlinkedModelSerializer):
@@ -38,14 +38,15 @@ class UserAccountSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserAccount
         fields = (
-        "user_group", "user", "occupation", "bio", "profile_pic", "date_of_birth", "term_condition_accept_state",
-        "safety_guide_accept_state")
+            "user_group", "user", "occupation", "bio", "profile_pic", "date_of_birth", "term_condition_accept_state",
+            "safety_guide_accept_state")
 
 
 class InsectGroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = InsectGroup
-        fields = ('group_id','group_name')
+        fields = ('group_id', 'group_name')
+
 
 class InsectSerializer(serializers.HyperlinkedModelSerializer):
     insect_group = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -54,16 +55,21 @@ class InsectSerializer(serializers.HyperlinkedModelSerializer):
         model = Insect
         fields = ('insect_id', 'insect_name', 'insect_desc', 'insect_group', 'insect_image_path')
 
+
 class AllInsectUserUploadSerializer(serializers.HyperlinkedModelSerializer):
+    sample_record_data = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = AllInsectUserUpload
-        fields = ('insect_id', 'insect_image_path')
+        fields = ('all_insect_image_id', 'sample_record_data', 'insect_image_path')
 
 
 class RiverEnvironmentImageSerializer(serializers.HyperlinkedModelSerializer):
+    sample_record_data = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = RiverEnvironmentImage
-        fields = ('river_id', 'river_image_path')
+        fields = ('river_environment_image_id', 'sample_record_data', 'river_image_path')
 
 
 class SampleRecordDataSerializer(serializers.HyperlinkedModelSerializer):
@@ -73,7 +79,8 @@ class SampleRecordDataSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = SampleRecord
-        fields = ('sample_id', 'sample_date', 'sample_score', 'sample_user', 'sample_pH', 'sample_tmp','sample_survey', 'sample_river',)
+        fields = ('sample_id', 'sample_date', 'sample_score', 'sample_user', 'sample_pH', 'sample_tmp', 'sample_survey',
+                  'sample_river',)
 
 
 class SampleRecordInsectDetailSerializer(serializers.HyperlinkedModelSerializer):
@@ -84,7 +91,8 @@ class SampleRecordInsectDetailSerializer(serializers.HyperlinkedModelSerializer)
         model = SampleRecordInsectDetail
         fields = ('sample_record_data', 'sample_record_insect', 'insect_number')
 
+
 class ReportProblemRecordSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ReportProblemRecord
-        fields = ('report_id','report_image_path','report_problem','report_problem_description')
+        fields = ('report_id', 'report_image_path', 'report_problem', 'report_problem_description')
