e9dcc64d09a98b09d03549daa93470969b6432e0
callMeDrStrange <bryan.liow.zy@gmail.com>
2021-03-22 21:43:44 +0000

    listitem added to HelpScreen.js

--- mobile_ui/screens/HelpScreen.js
+++ mobile_ui/screens/HelpScreen.js
@@ -1,24 +1,121 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableHighlight } from 'react-native';
+import { ListItem, Icon, Avatar  } from 'react-native-elements';
+import { useTheme } from '@react-navigation/native';
 
-import {useTheme} from '@react-navigation/native';
+const HelpScreen = ({ navigation }) => {
+  const { colors } = useTheme();
 
-const HelpScreen = ({navigation}) => {
-  const {colors} = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    header: {
+      backgroundColor: '#ededed',
+      height: 150,
+      width: '100%',
+    },
+    headerText: {
+      paddingTop: 50,
+      paddingLeft: 20,
+      fontSize: 30,
+      color: '#424242',
+    },
+    headerTitle: {
+      height: 60,
+      width: '100%',
+    },
+    headerTitleText: {
+      paddingTop: 20,
+      paddingLeft: 20,
+      fontSize: 14,
+      color: '#424242',
+    },
+    title: {
+      color: 'black',
+    },
+    listContainer: {
+      backgroundColor: colors.background,
+      width: '100%',
+      height: 50,
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={{color: colors.text}}>Help Page</Text>
-      <Button title="Click Here" onPress={() => navigation.navigate('HelpScreenTakeSample')} />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Support</Text>
+      </View>
+      <View style={styles.headerTitle}>
+        <Text style={styles.headerTitleText}>Choose an area you need help</Text>
+      </View>
+      <View style={styles.listContainer}>
+
+      <ListItem
+          Component={TouchableHighlight}
+          containerStyle={{}}
+          disabledStyle={{ opacity: 0.5 }}
+          bottomDivider
+          topDivider
+          onPress={() => navigation.navigate('HelpScreenTakeSample')}
+          pad={20}
+        >
+          <ListItem.Content>
+            <ListItem.Title>
+              <Text>Taking Sample</Text>
+            </ListItem.Title>
+            {/* <ListItem.Subtitle>
+              <Text>React Native Elements</Text>
+            </ListItem.Subtitle> */}
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+
+        <ListItem
+          Component={TouchableHighlight}
+          containerStyle={{}}
+          disabledStyle={{ opacity: 0.5 }}
+          bottomDivider
+          topDivider
+          onPress={() => navigation.navigate('HelpScreenTakeSample')}
+          pad={20}
+        >
+          <ListItem.Content>
+            <ListItem.Title>
+              <Text>Viewing Sample</Text>
+            </ListItem.Title>
+            {/* <ListItem.Subtitle>
+              <Text>React Native Elements</Text>
+            </ListItem.Subtitle> */}
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+
+        <ListItem
+          Component={TouchableHighlight}
+          containerStyle={{}}
+          disabledStyle={{ opacity: 0.5 }}
+          bottomDivider
+          topDivider
+          onPress={() => navigation.navigate('HelpScreenTakeSample')}
+          pad={20}
+        >
+          <ListItem.Content>
+            <ListItem.Title>
+              <Text>Setting</Text>
+            </ListItem.Title>
+            {/* <ListItem.Subtitle>
+              <Text>React Native Elements</Text>
+            </ListItem.Subtitle> */}
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+      </View>
     </View>
   );
 };
 
 export default HelpScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
