70d02842d10ab16a6ff2ef1d6cb12a35051d8d0b
Wenyu Shi <d00198805@student.dkit.ie>
2021-04-04 19:41:55 +0100

    add new line

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # RELEASE KEYSTORE
-echo "$RELEASE_KEYSTORE" > my-release-key.keystore.asc
-gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch my-release-key.keystore.asc > mobile_ui/android/app/my-release-key.keystore
+# echo "$RELEASE_KEYSTORE" > my-release-key.keystore.asc
+# gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch my-release-key.keystore.asc > mobile_ui/android/app/my-release-key.keystore
 
 
 
 # set android/gradle.properties
-echo "$GRADLE_PROPERTIES" >> mobile_ui/android/gradle.properties
+echo $GRADLE_PROPERTIES | tr ' ' '\n' >> gradle.properties
 
 
