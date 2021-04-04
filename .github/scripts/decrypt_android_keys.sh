dc7c2f4aa88d0ac934a7d009ca791801b5265a61
Wenyu Shi <d00198805@student.dkit.ie>
2021-04-04 17:44:33 +0100

    file_modify gradle.properties

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -6,7 +6,6 @@ cat my-release-key.keystore.asc
 gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch my-release-key.keystore.asc > mobile_ui/android/app/my-release-key.keystore
 
 # list android/app
-ls mobile_ui/android/app/
 
 # set android/gradle.properties
-echo "$GRADLE_PROPERTIES" >> mobile_ui/android/gradle.properties
+# echo "$GRADLE_PROPERTIES" >> mobile_ui/android/gradle.properties
