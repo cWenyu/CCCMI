237e5234df240ccf708a3907524b0d4f3cf93725
Wenyu Shi <d00198805@student.dkit.ie>
2021-04-04 17:18:22 +0100

    Generate Release APK/bundle file for project

---
+++ .github/scripts/decrypt_android_keys.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# RELEASE KEYSTORE
+echo "$RELEASE_KEYSTORE" > my-release-key.keystore.asc
+cat my-release-key.keystore.asc
+gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch my-release-key.keystore.asc > mobile_ui/android/app/my-release-key.keystore
+
+# set android/gradle.properties
+echo "$GRADLE_PROPERTIES" >> mobile_ui/android/gradle.properties
