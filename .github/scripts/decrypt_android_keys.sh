8c3bdc5e169373d3cbca898b0e6a510a4cb7cd38
Wenyu Shi <d00198805@student.dkit.ie>
2021-04-04 19:10:02 +0100

    Sign AAB

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -4,7 +4,9 @@
 echo "$RELEASE_KEYSTORE" > my-release-key.keystore.asc
 gpg -d --passphrase="$RELEASE_KEYSTORE_PASSPHRASE" --batch my-release-key.keystore.asc > mobile_ui/android/app/my-release-key.keystore
 
-# list android/app
+
 
 # set android/gradle.properties
-# echo "$GRADLE_PROPERTIES" >> mobile_ui/android/gradle.properties
+echo "$GRADLE_PROPERTIES" >> mobile_ui/android/gradle.properties
+
+
